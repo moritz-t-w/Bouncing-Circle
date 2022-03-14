d59dc6b2fe926bb40ee6223465508294d706d78b
MoDerPo <90388353+moritz-t-w@users.noreply.github.com>
2022-03-14 12:19:17 +0100

    circle drawn.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
 function setup() {
     createCanvas(600, 400);
+    background("#000");
+    stroke("#fff")
+    strokeWeight(4)
+    fill("#000")
+    circle(600 / 2, 400 / 2, 100)
 }
 
 function draw() {
-    background(0, 0, 0);
+
 }

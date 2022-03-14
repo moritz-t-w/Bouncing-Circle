6b960de605f9e7addcf5d52dedc6a6e2ddc88698
MoDerPo <90388353+moritz-t-w@users.noreply.github.com>
2022-03-14 12:21:51 +0100

    added x and y as vars

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
+let x, y
+
 function setup() {
     createCanvas(600, 400);
     background("#000");
     stroke("#fff")
     strokeWeight(4)
     fill("#000")
+    x = 600 / 2
+    y = 400 / 2
 }
 
 function draw() {
